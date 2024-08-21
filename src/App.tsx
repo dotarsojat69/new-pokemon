8eb48725891ffe233d180b7369edd8fb96a3c2a4
dotarsojat69 <tdeath457@gmail.com>
2024-08-21 15:47:56 +0700

    feat: add route on navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import { Background } from './components/Background'
+import "./scss/index.scss"
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Pokemon from './pages/Pokemon'
+import Compare from './pages/Compare'
+import MyList from './pages/MyList'
+import Search from './pages/Search'
 
 const App = () => {
   return (
-    <div>
-      App
+    <div className='main-container'>
+      <Background />
+      <BrowserRouter>
+      <div className='app'>
+        <Navbar />
+        <Routes>
+              <Route element={<Search />} path="/search" />
+              <Route element={<MyList />} path="/list" />
+              <Route element={<Compare />} path="/compare" />
+              <Route element={<Pokemon />} path="/pokemon/:id" />
+              <Route element={<Navigate to="/pokemon/1" />} path="*" />
+            </Routes>
+        <Footer />
+      </div>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
 export default App
